86ba30326bd2cf5fa0e93bcf76d813600506599f
MATEUSPIRES <daniel.m.p@hotmail.fr>
2015-10-01 11:24:26 +0100

    implemented destructor

---
+++ CAProjectSolution/CAProject/File1.cpp
@@ -0,0 +1,5 @@
+//this file is the same as file 2
+//Some code
+int test1(){
+	return 0;
+}
---
+++ CAProjectSolution/CAProject/File2.cpp
@@ -0,0 +1,8 @@
+//this file is the same as file 1
+//Some code
+int test2(){
+	return 1;
+}
+//test some differences
+//some test
+//some other line
